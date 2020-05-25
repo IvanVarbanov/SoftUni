2aa5ce46b47b9f47cd470b685fb0eb5613bea356
Ivan Varbanov <ivan@varbanov.net>
2020-05-25 15:44:41 +0300

    Solving problem 6

--- JavaScript/04.JavaScript-Applications/05.Object-Composition-Lab/06.Sum/app.js
+++ JavaScript/04.JavaScript-Applications/05.Object-Composition-Lab/06.Sum/app.js
@@ -1,9 +1,24 @@
-function init(id1, id2, resultId) {
+function result() {
     return {
-        id1,
-        id2,
-        resultId,
-        add: () => {},
-        subtract: () => {},
+        id1: '',
+        id2: '',
+        resultId: '',
+        init: function (id1, id2, resultId) {
+            this.id1 = id1;
+            this.id2 = id2;
+            this.resultId = resultId;
+        },
+        add: function () {
+            let input1 = document.querySelector(this.id1).value;
+            let input2 = document.querySelector(this.id2).value;
+            let res = document.querySelector(this.resultId);
+            res.value = Number(input1) + Number(input2);
+        },
+        subtract: function () {
+            let input1 = document.querySelector(this.id1).value;
+            let input2 = document.querySelector(this.id2).value;
+            let res = document.querySelector(this.resultId);
+            res.value = Number(input1) - Number(input2);
+        },
     };
 }
