805ff86e2a4766fd53208406a098e85df5fa52b4
Ivan Varbanov <ivan@varbanov.net>
2020-09-03 14:12:19 +0300

    Update bugs

--- JavaScript/05.JavaScript-Back-End/03.Intro-to-node.js/01.Cat-Shelter/handlers/static-files.js
+++ JavaScript/05.JavaScript-Back-End/03.Intro-to-node.js/01.Cat-Shelter/handlers/static-files.js
@@ -26,22 +26,46 @@ module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
     if (pathname.startsWith('/content') && req.method === 'GET') {
-        fs.readFile(`./${pathname}`, 'utf-8', (err, data) => {
-            if (err) {
-                console.log(err);
-                res.writeHead(404, {
+        if (
+            pathname.endsWith('png') ||
+            pathname.endsWith('jpg') ||
+            pathname.endsWith('jpeg') ||
+            pathname.endsWith('ico')
+        ) {
+            fs.readFile(`./${pathname}`, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.writeHead(404, {
+                        'Content-type': getContentType(pathname),
+                    });
+                    res.write('Page not found!');
+                    res.end();
+                    return;
+                }
+                res.writeHead(200, {
                     'Content-type': getContentType(pathname),
                 });
-                res.write('Page not found!');
+                res.write(data);
                 res.end();
-                return;
-            }
-            res.writeHead(200, {
-                'Content-type': getContentType(pathname),
             });
-            res.write(data);
-            res.end();
-        });
+        } else {
+            fs.readFile(`./${pathname}`, 'utf-8', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.writeHead(404, {
+                        'Content-type': getContentType(pathname),
+                    });
+                    res.write('Page not found!');
+                    res.end();
+                    return;
+                }
+                res.writeHead(200, {
+                    'Content-type': getContentType(pathname),
+                });
+                res.write(data);
+                res.end();
+            });
+        }
     } else {
         return true;
     }
